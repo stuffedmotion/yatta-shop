40f3f27f787cc798079f3d116a26bba0d786e438
Tyler Pearce <tyler@stuffedmotion.ca>
2020-05-15 16:45:30 -0400

    color

--- src/components/Product/ProductOption/index.tsx
+++ src/components/Product/ProductOption/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cx from 'classnames'
 import { getMetafield } from '@utils/getMetafield'
 import { ShopifyProduct, ShopifyProductOption } from '@typings/storefront'
@@ -23,6 +23,19 @@ interface ProductOptionProps {
 //     {variant.title} | {getMetafield(`variant_color`, variant.metafields)}
 // </button>
 
+
+// const updateColor = (color: string) => {
+//   const matchedVariant = product.variants.find(variant =>
+//     variant.selectedOptions.find(
+//       option => option.name === `Color` && option.value === color
+//     )
+//   )
+//   if (sliderRef && matchedVariant)
+//     sliderRef.current.goToImageId(matchedVariant.image?.id)
+
+
+// UPDATE STATE AND IAMGE SLIDER FROM PRODUCT PAGE FOR ALL VARIANTS
+
 const optionRenderer = (props: ProductOptionProps) => {
   const {
     option: { name },
@@ -40,24 +53,37 @@ const optionRenderer = (props: ProductOptionProps) => {
 const ColorOption = (props: ProductOptionProps) => {
   const { option, sliderRef, product } = props
 
+  const [selectedColor, setSelectedColor] = useState(option.values[0])
+
+    setSelectedColor(color)
+  }
+
   const colors = getMetafield(`product_colors`, product.metafields)
   if (!colors) return
 
   const colorMap = JSON.parse(colors)
-
-  return option.values.map(value => {
-    return (
-      <div key={value} className={cx(styles.optionWrapper, styles.colorOption)}>
-        <button type="button" className={styles.color}>
-          <span
-            style={{ backgroundColor: colorMap[value] || `#FFFFFF` }}
-            className={styles.colorBox}
-          />
-        </button>
-        {value}
-      </div>
-    )
-  })
+  return (
+    <div className={cx(styles.optionWrapper, styles.colorOption)}>
+      {option.values.map(value => {
+        return (
+          <button
+            key={value}
+            type="button"
+            onClick={() => updateColor(value)}
+            className={cx(styles.color, {
+              [styles.active]: value === selectedColor,
+            })}
+          >
+            <div
+              style={{ backgroundColor: colorMap[value] || `#FFFFFF` }}
+              className={styles.colorBox}
+            />
+            <div>{value}</div>
+          </button>
+        )
+      })}
+    </div>
+  )
 }
 
 const ProductOption = (props: ProductOptionProps) => {
